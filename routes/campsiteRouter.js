571628f27ab21f02c4b287e2d6bf91c75fce7afd
Lgenteman <lydiagenteman@gmail.com>
2024-07-07 19:53:25 -0500

    Week 3 Workshop-verifyAdmin(), get/users, verifyUser() to comments

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -138,7 +138,7 @@ campsiteRouter.route('/:campsiteId')
     });
 
     campsiteRouter.route('/:campsiteId/comments/:commentId')
-.get((req, res, next) => {
+    .get((req, res, next) => {
     Campsite.findById(req.params.campsiteId)
     .populate('comments.author')
     .then(campsite => {
@@ -158,27 +158,35 @@ campsiteRouter.route('/:campsiteId')
     })
     .catch(err => next(err));
 })
+
 .post(authenticate.verifyUser, (req, res) => {
     res.statusCode = 403;
     res.end(`POST operation not supported on /campsites/${req.params.campsiteId}/comments/${req.params.commentId}`);
 })
+
 .put(authenticate.verifyUser, (req, res, next) => {
     Campsite.findById(req.params.campsiteId)
     .then(campsite => {
         if (campsite && campsite.comments.id(req.params.commentId)) {
-            if (req.body.rating) {
-                campsite.comments.id(req.params.commentId).rating = req.body.rating;
+            if ((campsite.comments.id(req.params.commentId).author._id).equals(req.user._id)) {
+                if (req.body.rating) {
+                    campsite.comments.id(req.params.commentId).rating = req.body.rating;
+                }
+                if (req.body.text) {
+                    campsite.comments.id(req.params.commentId).text = req.body.text;
+                }
+                campsite.save()
+                .then(campsite => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(campsite);
+                })
+                .catch(err => next(err));
+            } else {
+                err = new Error('You are not authorized to update this comment!')
+                err.statusCode = 403; 
+                return next(err);
             }
-            if (req.body.text) {
-                campsite.comments.id(req.params.commentId).text = req.body.text;
-            }
-            campsite.save()
-            .then(campsite => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(campsite);
-            })
-            .catch(err => next(err));
         } else if (!campsite) {
             err = new Error(`Campsite ${req.params.campsiteId} not found`);
             err.status = 404;
@@ -191,18 +199,25 @@ campsiteRouter.route('/:campsiteId')
     })
     .catch(err => next(err));
 })
+
 .delete(authenticate.verifyUser, (req, res, next) => {
     Campsite.findById(req.params.campsiteId)
     .then(campsite => {
         if (campsite && campsite.comments.id(req.params.commentId)) {
-            campsite.comments.id(req.params.commentId).deleteOne();
-            campsite.save()
-            .then(campsite => {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(campsite);
-            })
-            .catch(err => next(err));
+            if ((campsite.comments.id(req.params.commentId).author._id).equals(req.user._id)) {
+                campsite.comments.id(req.params.commentId).remove();
+                campsite.save()
+                .then(campsite => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json(campsite);
+                })
+                .catch(err => next(err));
+            } else {
+                err = new Error('You are not authorized to delete this comment!')
+                err.statusCode = 403; 
+                return next(err);
+            }
         } else if (!campsite) {
             err = new Error(`Campsite ${req.params.campsiteId} not found`);
             err.status = 404;
@@ -215,5 +230,6 @@ campsiteRouter.route('/:campsiteId')
     })
     .catch(err => next(err));
 });
+
 
 module.exports = campsiteRouter;
